a10fceaf97051824f2f480327648a04d709ef6d2
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-10-08 10:41:19 +0200

    isolate header creation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::prelude::*;
 use std::net::{Shutdown, TcpStream};
+
+use std::collections::HashMap;
 use std::str;
 
 fn from_bytes_to_str(buf: &[u8]) -> &str {
@@ -9,21 +11,39 @@ fn from_bytes_to_str(buf: &[u8]) -> &str {
     }
 }
 
-fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8938")?;
+fn create_header(method: &str, params: HashMap<&str, &str>) -> String {
+    let mut result = String::from(format!("{method} / HTTP/1.1\n"));
 
-    stream.write(b"GET / HTTP/1.1\nHost: localhost\n\n")?;
+    for (key, value) in &params {
+        result.push_str(&format!("{key}: {value}\n"));
+    }
 
+    result.push_str("\n");
+    return result;
+}
+
+fn get(url: &str, params: HashMap<&str, &str>) -> std::io::Result<()> {
+    let mut stream = TcpStream::connect(url)?;
+
+    let header = create_header("GET", params);
+    stream.write(header.as_bytes())?;
+
+    // get the header
     let mut result: [u8; 8000] = [0; 8000];
     stream.read(&mut result)?;
     let result = from_bytes_to_str(&result);
     println!("{result}");
 
-    let mut result: [u8; 336] = [0; 336];
+    // get the content
+    let mut result: [u8; 8000] = [0; 8000];
     stream.read(&mut result)?;
     let result = from_bytes_to_str(&result);
     println!("{result}");
 
     stream.shutdown(Shutdown::Both)?;
     Ok(())
+}
+
+fn main() {
+    let _ = get("localhost:8938", HashMap::from([("Host", "localhost")]));
 }

60ede955b26d9dd3ac657b0a52dbaab8ac6f46d3
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-10-15 10:22:41 +0200

    http reader

--- src/main.rs
+++ src/main.rs
@@ -22,28 +22,71 @@ fn create_header(method: &str, params: HashMap<&str, &str>) -> String {
     return result;
 }
 
+fn bytes_headers_to_hashtable(headers: &[u8; 8000]) -> HashMap<&str, &str> {
+    let mut new_headers: HashMap<&str, &str> = HashMap::new();
+    let headers = from_bytes_to_str(headers);
+    for (i, line) in headers.split("\n").enumerate() {
+        // first line
+        if i == 0 {
+            let mut words = line.split(" ");
+            let http_version = words.nth(0).unwrap().trim();
+            let status = words.nth(0).unwrap().trim();
+            new_headers.insert("status", status);
+            new_headers.insert("http-version", http_version);
+            continue;
+        }
+        // end of the headers
+        if line == "\r" {
+            break;
+        }
+        // if line not valid
+        if !line.contains(":") {
+            println!("error line {i}");
+            println!("line: {:?}", line);
+            new_headers.insert("error", "http header is invalid");
+            continue;
+        }
+
+        // TODO: make the parameter case insensitive
+        let index = line.find(":").unwrap();
+        let parameter = line[0..index].trim();
+        let value = line[index + 1..].trim();
+        new_headers.insert(parameter, value);
+    }
+
+    new_headers
+}
+
 fn get(url: &str, params: HashMap<&str, &str>) -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(url)?;
+    match TcpStream::connect(url) {
+        Ok(mut stream) => {
+            let header = create_header("GET", params);
+            stream.write(header.as_bytes())?;
 
-    let header = create_header("GET", params);
-    stream.write(header.as_bytes())?;
+            // get the header
+            let mut result: [u8; 8000] = [0; 8000];
+            stream.read(&mut result)?;
+            let response_headers = bytes_headers_to_hashtable(&result);
+            println!("{:#?}", response_headers);
+            let result = from_bytes_to_str(&result);
+            println!("{result}");
 
-    // get the header
-    let mut result: [u8; 8000] = [0; 8000];
-    stream.read(&mut result)?;
-    let result = from_bytes_to_str(&result);
-    println!("{result}");
+            // get the content
+            let mut result: [u8; 8000] = [0; 8000];
+            stream.read(&mut result)?;
+            let result = from_bytes_to_str(&result);
+            println!("{result}");
 
-    // get the content
-    let mut result: [u8; 8000] = [0; 8000];
-    stream.read(&mut result)?;
-    let result = from_bytes_to_str(&result);
-    println!("{result}");
-
-    stream.shutdown(Shutdown::Both)?;
-    Ok(())
+            stream.shutdown(Shutdown::Both)?;
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
 }
 
 fn main() {
-    let _ = get("localhost:8938", HashMap::from([("Host", "localhost")]));
+    let _ = match get("localhost:8000", HashMap::from([("Host", "localhost")])) {
+        Ok(x) => println!("{:?}", x),
+        Err(x) => println!("{:?}", x),
+    };
 }
